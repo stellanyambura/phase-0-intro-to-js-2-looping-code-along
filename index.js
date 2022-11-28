8baf31d6a67ecd8a961e6451ad1048cf6595a434
stellagitonga <stellanyamburagitonga@gmail.com>
2022-11-28 16:04:11 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    //  so as to return in an array
+    const message_array = [];
+  
+    for (let i = 0; i < name.length; i++) {
+      // "Thank you, Charlie, for the wonderful birthday gift!"
+      const message =
+        "Thank you, " + name[i] + ", for the wonderful " + event + " gift!";
+      message_array.push(message);
+    }
+  
+    return message_array;
+  }
+  
+  function countDown(num) {
+    do {
+      console.log(num);
+      num--;
+    } while (num >= 0);
+  }
